79ba942aa0d2879a5f7fb2067fd73520cefe74fe
happy-turtle-stack <braedon.stapelman@drake.edu>
2026-03-10 10:36:25 -0500

    created read_my_table.py and implemented the functions to print out the items in the Songs table

---
+++ read_my_table.py
@@ -0,0 +1,35 @@
+import boto3
+from boto3.dynamodb.conditions import Key
+
+REGION = "us-east-1"
+TABLE_NAME = "Songs"
+
+def get_table():
+    #returns a reference to the dynamodb Music table
+
+    dynamodb = boto3.resource("dynamodb", region_name=REGION)
+    return dynamodb.Table(TABLE_NAME)
+
+def print_song(song):
+    #prints the song passed to it
+    name = song.get("Name", "Unknown Title")
+    artist = song.get("Artist", "Unknown Artist")
+
+    print(f"  Name  : {name}")
+    print(f"  Artist: {artist}")
+    print()
+
+def print_all_songs():
+    #prints all songs in the table
+    table = get_table()
+
+    response = table.scan()
+    items = response.get("Items", [])
+    
+    if not items:
+        print("No songs found. Make sure DynamoDB is connecter correctly and contains items")
+        return
+    
+    print(f"Found {len(items)} song(s): \n")
+    for song in items:
+        print_song(song)
